9f91eeb1a6f4eba292695a0b388c350c795726c7
mzuelch <76059155+mzuelch@users.noreply.github.com>
2022-08-24 16:53:36 +0200

    Update

---
+++ Modules/HAGIWO/4Ch Sample Player/Firmware/Original/HAGIWO_4Ch_Sampler/samples.h
@@ -0,0 +1,45 @@
+const static char* smpl_name[] PROGMEM = {//display sample name
+ "808 BD ", "808 SD ", "808 TOM", "808 RIM", "808 HC ",  "808 CB ", "808 CH ", "808 OH ", "909 BD1", "909 BD2", "909 SD1", "909 SD2", "909 TOM", "909 RIM", "909 HC1", "909 HC2", "909 CH ", "909 OH "
+};
+
+// Sample data omitted due to copyright issues
+// Use WAV2VAR to convert your own samples
+
+const static byte smpl[18][8000] PROGMEM =  {
+  // 808 BD
+  {..},
+  // 808 SD
+  {..},
+  // 808 TOM
+  {..},
+  // 808 RIM
+  {..},
+  // 808 HC
+  {..},
+  // 808 CB
+  {..},
+  // 808 CH
+  {..},
+  // 808 OH
+  {..},
+  // 909 BD1
+  {..},
+  // 909 BD2
+  {..},
+  // 909 SD1
+  {..},
+  // 909 SD2
+  {..},
+  // 909 TOM
+  {..},
+  // 909 RIM
+  {..},
+  // 909 HC1
+  {..},
+  // 909 HC2
+  {..},
+  // 909 CH
+  {..},
+  // 909 OH
+  {..}
+};
